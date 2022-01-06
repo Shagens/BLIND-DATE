a0653d049ad2fc0d0eb6a093d551299eb61999d6
Titoxox97 <87781987+Titoxox97@users.noreply.github.com>
2022-01-05 20:00:04 -0500

    edit backend

--- API/index.js
+++
@@ -1,7 +0,0 @@
-const express = require("express");
-const mongoose = require("express");
-//App Config
-const app = express();
-const port = process.env.PORT || 5001;
-
-app.listen(port, () => console.log(`Listening on localhost: ${port}`));
