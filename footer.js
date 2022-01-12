18894a3d9f0df3597365c467dee4f9399ebd7129
Titoxox97 <87781987+Titoxox97@users.noreply.github.com>
2022-01-12 15:13:21 -0500

    Create footer.js
    footer

---
+++ footer.js
@@ -0,0 +1,29 @@
+<div className="container">
+        <ul className="list-unstyled list-inline text-center">
+          <li className="list-inline-item">
+            <a
+              className="btn-floating btn-fb mx-1"
+              href="https://github.com/Titoxox97"
+            >
+              <i
+                className="fa fa-github"
+                style={{
+                  color: "black",
+                  fontSize: "24px",
+                  justifyContent: "center",
+                }}
+              ></i>
+            </a>
+          </li>
+          <li className="list-inline-item">
+            <a
+              className="btn-floating btn-li mx-1"
+              href="https://www.linkedin.com/in/matias-ahrensdorf-5353b4152"
+            >
+              <i className="fa fa-linkedin" style={{ color: "black" }}>
+                {" "}
+              </i>
+            </a>
+          </li>
+        </ul>
+      </div>
