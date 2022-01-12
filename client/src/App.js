cf224ae1ee70cdbc7b9c6ba9f9fcd2606c820cb9
Shagens <Hagensshik@gmail.com>
2022-01-11 22:37:06 -0500

    added login and signup

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,11 @@
-import React, {useState, useRef, useEffect} from 'react';
+import React, {useState, useEffect} from 'react';
 import Header from './components/Header';
+import Datingcards from './components/DatingCards';
 import io from 'socket.io-client';
 import TextField from '@material-ui/core/TextField';
 import { ApolloClient, InMemoryCache, ApolloProvider, useQuery, gql} from "@apollo/client";
+import LoginForm from './components/LoginForm';
+import SignupForm from './components/SignupForm';
 // import ChatAndSearch  from './Components/Landing/Search/ChatAndSearch';
 
 const client = new ApolloClient({
@@ -21,105 +24,71 @@ const client = new ApolloClient({
 //   },
 //   uri: '/graphql',
 // })
+const socket = io.connect('http://localhost:3001')
 
+const App = () => {
+  const [state, setState] = useState({
+    message: '', name: ''
+  }) 
 
+  useEffect(() => {
+    socket.on('message', ({name, message }) => {
+      setChat([...chat, { name, message }])
+    })
+  })
 
-function App() {
-	const [ state, setState ] = useState({ message: "", name: "" })
-	const [ chat, setChat ] = useState([])
+  const onTextChange = e => {
+    setState({...state, [e.target.name]: e.target.value})
+  }
 
-	const socketRef = useRef()
+  const onMessageSubmit = (e) => {
+    e.preventDefault()
+    const {name, message} = state
+    socket.emit('message', {name, message })
+    setState({ message: '', name })
+  }
 
-	useEffect(
-		() => {
-			socketRef.current = io.connect("http://localhost:4000")
-			socketRef.current.on("message", ({ name, message }) => {
-				setChat([ ...chat, { name, message } ])
-			})
-			return () => socketRef.current.disconnect()
-		},
-		[ chat ]
-	)
+  const renderChat = () => {
+    return chat.map(({name, message}, index) =>(
+      <div key={index}>
+        <h3>
+          {name}: <span>{message}</span>
+        </h3>
+      </div>
+    ))
+  }
 
-	const onTextChange = (e) => {
-		setState({ ...state, [e.target.name]: e.target.value })
-	}
-
-	const onMessageSubmit = (e) => {
-		const { name, message } = state
-		socketRef.current.emit("message", { name, message })
-		e.preventDefault()
-		setState({ message: "", name })
-	}
-
-	const renderChat = () => {
-		return chat.map(({ name, message }, index) => (
-			<div key={index}>
-				<h3>
-					{name}: <span>{message}</span>
-				</h3>
-			</div>
-		))
-	}
-
-	return (
-		<div className="card">
-			<form onSubmit={onMessageSubmit}>
-				<h1>Messenger</h1>
-				<div className="name-field">
-					<TextField name="name" onChange={(e) => onTextChange(e)} value={state.name} label="Name" />
-				</div>
-				<div>
-					<TextField
-						name="message"
-						onChange={(e) => onTextChange(e)}
-						value={state.message}
-						id="outlined-multiline-static"
-						variant="outlined"
-						label="Message"
-					/>
-				</div>
-				<button>Send Message</button>
-			</form>
-			<div className="render-chat">
-				<h1>Chat Log</h1>
-				{renderChat()}
-			</div>
-		</div>
-	)
-
-  const [currentPage, handlePageChange] = useState('Header');
-  const renderPage = () => {
-    switch (currentPage) {
-      case 'Header':
-        return <Header />;
-      // case 'Portfolio':
-      //   return <Portfolio />;
-      // case 'Contactme':
-      //   return <ContactForm />;
-      //   case 'Resume':
-      //   return <Resume />
-      // default:
-      //   return <Aboutme />;
-    }
-  };
+  const [chat, setChat] = useState([])
   return (
-    <div>
-    <Header handlePageChange={handlePageChange}/>
-    {/* <Nav/>
-    <div>{renderPage(currentPage)}</div>
-    {/* <Aboutme/>
-    <Portfolio/>
-    <Resume/>
-    <ContactForm/> */}
-    {/* <Footer/> */} 
-    </div>
-    
-  );
+    <div className="card">
+      <Header />
+      <LoginForm/>
+      <form onSubmit={onMessageSubmit}>
+        <h1>Message</h1>
+        <div className="name-field">
+        <TextField
+          name="name"
+          onChange={e => onTextChange(e)}
+          value={state.name}
+          label="Name" />
+      </div>
+
+          <div>
+          <TextField
+            name="name"
+            onChange={e => onTextChange(e)}
+            value={state.message}
+            id="outlined-multiline-static"
+            variant="outlined"
+            label="Message" />
+            </div>
+            <button>Send</button>
+        </form>
+        <div className="render-chat"></div>
+        <h1> Chat log</h1>
+        {renderChat()}
+      </div>
+  )
 }
-
-
-
-
 
 export default App;
